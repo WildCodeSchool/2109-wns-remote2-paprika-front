e659a6737ee6aab418c71097a27a766392563caf
HEMMER-FX <hemmerfrancoisxavier@gmail.com>
2021-10-28 15:51:27 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import logoPaprika from './assets/pictures/paprika1.png';
+import './assets/App.css';
+const red = {color:'red'}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <img src={logoPaprika} className="App-logo" alt="logo" />
+          <h1 style={red}>Projet paprika</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://drive.google.com/drive/folders/1OxO-xvH8QRMulYSm_nZP9imbOXgQNOUl"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          drive projet
         </a>
       </header>
     </div>
