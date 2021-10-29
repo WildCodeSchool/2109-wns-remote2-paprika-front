29114af5a01990096b6d4e9e805c152b62da1fe5
HEMMER-FX <hemmerfrancoisxavier@gmail.com>
2021-10-29 10:55:25 +0200

    fix pretier eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logoPaprika from './assets/pictures/paprika1.png';
-import './assets/App.css';
-const red = {color:'red'}
+import React from 'react'
+import logoPaprika from './assets/pictures/paprika1.png'
+import './assets/App.css'
+const red = { color: 'red' }
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logoPaprika} className="App-logo" alt="logo" />
-          <h1 style={red}>Projet paprika</h1>
-        <a
-          className="App-link"
-          href="https://drive.google.com/drive/folders/1OxO-xvH8QRMulYSm_nZP9imbOXgQNOUl"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          drive projet
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logoPaprika} className="App-logo" alt="logo" />
+                <h1 style={red}>Projet paprika</h1>
+                <a
+                    className="App-link"
+                    href="https://drive.google.com/drive/folders/1OxO-xvH8QRMulYSm_nZP9imbOXgQNOUl"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    drive projet
+                </a>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
