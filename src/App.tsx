511814290c5c7558e131283a3e38a28f332f2a6e
HEMMER-FX <hemmerfrancoisxavier@gmail.com>
2021-11-02 21:46:57 +0100

    reset front

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react'
-import logoPaprika from './assets/pictures/paprika1.png'
-import './assets/App.css'
-import Nav from './components/Nav/Nav'
-const red = { color: 'red' }
+
 function App() {
     return (
-        <div className="App">
-            <Nav />
-            <header className="App-header">
-                <img src={logoPaprika} className="App-logo" alt="logo" />
-                <h1 style={red}>Projet paprika</h1>
-                <a
-                    className="App-link"
-                    href="https://drive.google.com/drive/folders/1OxO-xvH8QRMulYSm_nZP9imbOXgQNOUl"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    drive projet
-                </a>
-            </header>
+        <div>
+            <h1>test</h1>
         </div>
     )
 }
