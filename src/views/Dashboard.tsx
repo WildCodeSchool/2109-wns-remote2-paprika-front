d3e35e7b934a1446019122e240bd05b95d40dbbc
wonecode <lochlainn.gadault@gmail.com>
2022-06-23 11:57:56 +0200

    🚑 fix: router

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import Layout from '../components/Layout';
+import Page from '../components/Page';
+
 const Dashboard = () => {
-  return <h1>Dashboard page</h1>;
+  return (
+    <Page sx={{ height: '100vh' }} title="Dashboard">
+      <Layout>
+        <h1>Dashboard page</h1>
+      </Layout>
+    </Page>
+  );
 };
 
 export default Dashboard;
